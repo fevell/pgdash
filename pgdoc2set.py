6be27d84e630daa3ee975955c5864fc688e879cb
MattK <mksql@yahoo.com>
2012-05-18 18:12:14 -0400

    Additional filters added

--- pgdoc2set.py
+++ pgdoc2set.py
@@ -3,8 +3,8 @@
 import os, re, sqlite3
 from bs4 import BeautifulSoup, NavigableString, Tag 
 
-conn = sqlite3.connect('postgresql.docset/Contents/Resources/docSet.dsidx')
-cur = conn.cursor()
+db = sqlite3.connect('postgresql.docset/Contents/Resources/docSet.dsidx')
+cur = db.cursor()
 
 try: cur.execute('DROP TABLE searchIndex;')
 except: pass
@@ -19,11 +19,10 @@ soup = BeautifulSoup(page)
 any = re.compile('.*')
 for tag in soup.find_all('a', {'href':any}):
     name = tag.text.strip()
-    if len(name) > 1:
-        path = tag.attrs['href'].strip()
-        if path != 'index.html':
-            cur.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (name, 'func', path))
-            print 'name: %s, path: %s' % (name, path)
+    path = tag.attrs['href'].strip()
+    if path not in ('index.html', 'biblio.html', 'bookindex.html'):
+        cur.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (name, 'func', path))
+        print 'name: %s, path: %s' % (name, path)
 
-conn.commit()
-conn.close()
+db.commit()
+db.close()
