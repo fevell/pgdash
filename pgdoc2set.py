d2bb663fb88e0475f2db349ed8a8038db783d97c
MattK <mksql@yahoo.com>
2012-05-20 20:44:17 -0400

    Fixes #1: filter anchor on references to bookindex.html

--- pgdoc2set.py
+++ pgdoc2set.py
@@ -21,7 +21,7 @@ for tag in soup.find_all('a', {'href':any}):
     name = tag.text.strip()
     if len(name) > 0:
         path = tag.attrs['href'].strip()
-        if path not in ('index.html', 'biblio.html', 'bookindex.html'):
+        if path.split('#')[0] not in ('index.html', 'biblio.html', 'bookindex.html'):
             cur.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (name, 'func', path))
             print 'name: %s, path: %s' % (name, path)
 
